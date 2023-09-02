705fe800f9abf751cbebf9916d8208c6a4673cf0
Alexandre_Goulart <131091829+AlexandreGoular@users.noreply.github.com>
2023-09-02 20:25:52 -0300

    upload cronometro

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Selecione os elementos do cronômetro
+const horasElement = document.querySelector('.horas');
+const minutosElement = document.querySelector('.minutos');
+const segundosElement = document.querySelector('.segundos');
+const iniciarButton = document.querySelector('#iniciar');
+const pausarButton = document.querySelector('#pausar');
+const resetarButton = document.querySelector('#resetar');
