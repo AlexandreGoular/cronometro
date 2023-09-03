e788df5eba58c054131d175c9d8122160dd9184f
LinuxMistery00 <136195786+LinuxMistery00@users.noreply.github.com>
2023-09-02 21:58:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
-// Selecione os elementos do cronômetro
 const horasElement = document.querySelector('.horas');
 const minutosElement = document.querySelector('.minutos');
 const segundosElement = document.querySelector('.segundos');
 const iniciarButton = document.querySelector('#iniciar');
 const pausarButton = document.querySelector('#pausar');
 const resetarButton = document.querySelector('#resetar');
+
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+let intervalo;
+
+function atualizarCronometro() {
+    segundos ++;
+
+    if (segundos === 60) {
+        segundos = 0;
+        minutos++;
+
+        if (minutos === 60) {
+            minutos = 0;
+            horas++;
+        }
+    }
+
+    segundosElement.textContent = segundos.toString().padStart(2, '0');
+    minutosElement.textContent = minutos.toString().padStart(2, '0');
+    horasElement.textContent = horas.toString().padStart(2, '0');
+}
+
+iniciarButton.addEventListener('click', () => {
+    clearInterval(intervalo);
+    intervalo = setInterval(atualizarCronometro, 1000);
+});
+
+pausarButton.addEventListener('click', () => {
+    clearInterval(intervalo);
+});
+
+resetarButton.addEventListener('click', () => {
+    clearInterval(intervalo);
+    segundos = 0;
+    minutos = 0;
+    horas = 0;
+    segundosElement.textContent = '00';
+    minutosElement.textContent = '00';
+    horasElement.textContent = '00';
+});
